7691d13cfd93e696fb68f677ccff85e919fe3d3e
Riverajakob11 <jakobaindreas11@gmail.com>
2014-03-21 12:54:37 +0000

    Added new features.

--- src/clock.js
+++ src/clock.js
@@ -4,18 +4,20 @@ function init () {
   document.getElementById("clock").appendChild( timeDisplay );
 }
 
-function clock(){
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-  var timeOfDay = ( hours < 12 ) ? "AM" : "PM";
-  hours = ( hours > 12 ) ? hours - 12 : hours;
-  hours = ( hours === 0 ) ? 12 : hours;
-  minutes = ( minutes < 10 ? "0" : "" ) + minutes;
-  seconds = ( seconds < 10 ? "0" : "" ) + seconds;
-  var format = (hours + ":" + minutes + ":" + seconds + timeOfDay);
-  document.getElementById('clock').innerHTML = format;
-  setTimeout(clock,1000);
-}
+var clock = function(clockNode) {
+    return function() {
+        var currentTime = new Date(),
+        hours = currentTime.getHours(),
+        minutes = currentTime.getMinutes(),
+        seconds = currentTime.getSeconds(),
+        timeOfDay = ( hours < 12 ) ? "AM" : "PM";
+        hours = (hours > 12) ? hours - 12 : hours;
+        hours = (hours === 0) ? 12 : hours;
+	    minutes = (minutes < 10 ? "0" : "") + minutes;
+        seconds = (seconds < 10 ? "0" : "") + seconds;
+        var place = (hours + ":" + minutes + ":" + seconds + timeOfDay);
+        clockNode.innerHTML = place;
+    };
+}(document.getElementById('clock'));
+setInterval(clock, 1000);
 clock();
