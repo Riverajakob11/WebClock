87525bd20a2533c96d105df0ea90181c9134dd85
Riverajakob11 <jakobaindreas11@gmail.com>
2014-03-21 12:18:44 +0000

    fix bug on last change

--- src/clock.js
+++ src/clock.js
@@ -1,30 +1,21 @@
 //Clock.js Copyright © 2013 Jakob Aindreas A. Rivera
-function clock_container () {
-    function clock () {
-        function updateClock () {
-            var currentTime = new Date ();
-
-            var currentHours = currentTime.getHours ();
-            var currentMinutes = currentTime.getMinutes ();
-            var currentSeconds = currentTime.getSeconds ();
-
-            // Pad the minutes and seconds with leading zeros, if required
-            currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-            currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-            // Choose either "AM" or "PM" as appropriate
-            var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-            // Convert the hours component to 12-hour format if needed
-            currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-            // Convert an hours component of "0" to "12"
-            currentHours = ( currentHours === 0 ) ? 12 : currentHours;
-
-            // Compose the string for display
-            var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-        }
-    }
-    // Update the time display
-    document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+function init () {
+  timeDisplay = document.createTextNode ("");
+  document.getElementById("clock").appendChild( timeDisplay );
 }
+
+function clock(){
+  var currentTime = new Date();
+  var hours = currentTime.getHours();
+  var minutes = currentTime.getMinutes();
+  var seconds = currentTime.getSeconds();
+  var timeOfDay = ( hours < 12 ) ? "AM" : "PM";
+  hours = ( hours > 12 ) ? hours - 12 : hours;
+  hours = ( hours === 0 ) ? 12 : hours;
+  minutes = ( minutes < 10 ? "0" : "" ) + minutes;
+  seconds = ( seconds < 10 ? "0" : "" ) + seconds;
+  var format = (hours + ":" + minutes + ":" + seconds + timeOfDay);
+  document.getElementById('clock').innerHTML = format;
+  setTimeout(clock,1000);
+}
+clock();
