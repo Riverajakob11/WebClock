3efac44e048ba0749b26181a18e69a997f188dc9
Jakob Aindreas A. Rivera <Riverajakob11@users.noreply.github.com>
2014-03-22 13:11:48 +0800

    Update web-clock.js

--- src/web-clock.js
+++ src/web-clock.js
@@ -1,41 +1,29 @@
-//Clock.js Copyright © 2013 Jakob Aindreas A. Rivera
-function init () {
-  timeDisplay = document.createTextNode ("");
-  document.getElementById("clock").appendChild( timeDisplay );
-}
-
-<<<<<<< HEAD:src/clock.js
-var clock = function(clockNode) {
-    return function() {
-        var currentTime = new Date(),
-        hours = currentTime.getHours(),
-        minutes = currentTime.getMinutes(),
-        seconds = currentTime.getSeconds(),
-        timeOfDay = ( hours < 12 ) ? "AM" : "PM";
-        hours = (hours > 12) ? hours - 12 : hours;
-        hours = (hours === 0) ? 12 : hours;
-	    minutes = (minutes < 10 ? "0" : "") + minutes;
-        seconds = (seconds < 10 ? "0" : "") + seconds;
-        var place = (hours + ":" + minutes + ":" + seconds + timeOfDay);
-        clockNode.innerHTML = place;
+//WebClock. Copyright © 2014 Jakob Aindreas Rivera.
+/* Info:
+Version: 3.0
+Type: Widget
+Language: pure JavaScript
+Author: Jakob Aindreas Rivera
+*/
+//Note: The info comment is gonna be moved into a JSON format.
+var clock = (function clock(clockNode)
+{
+    clock = clock || document.querySelector('#clock');
+    //var currentTime,hours,minutes,seconds,timeOfDay;
+    return function()
+    {
+        /*currentTime = new Date();
+        hours = currentTime.getHours();
+        minutes = currentTime.getMinutes();
+        seconds = currentTime.getSeconds();
+        timeOfDay = (hours < 12) ? 'AM' : 'PM';*/
+        //clockNode.innerHTML = (hours + ":" + minutes + ":" + seconds + timeOfDay);
+        clockNode.replaceChild(
+            document.createTextNode(
+                (new Date()).toLocaleTimeString()
+            ),
+            clockNode.childNodes.item(0)
+        );
     };
-}(document.getElementById('clock'));
-setInterval(clock, 1000);
-clock();
-=======
-function clock(){
-  var currentTime = new Date();
-  var hours = currentTime.getHours();
-  var minutes = currentTime.getMinutes();
-  var seconds = currentTime.getSeconds();
-  var timeOfDay = ( hours < 12 ) ? "AM" : "PM";
-  hours = ( hours > 12 ) ? hours - 12 : hours;
-  hours = ( hours === 0 ) ? 12 : hours;
-  minutes = ( minutes < 10 ? "0" : "" ) + minutes;
-  seconds = ( seconds < 10 ? "0" : "" ) + seconds;
-  var format = (hours + ":" + minutes + ":" + seconds + timeOfDay);
-  document.getElementById('clock').innerHTML = format;
-  setTimeout(clock,1000);
-}
-clock();
->>>>>>> f379511ba6464e9467a546a15377db5c2ad481d3:src/web-clock.js
+}(document.getElementById('clock')));
+var clockInterval = setInterval(clock, 100);
